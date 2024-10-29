bbc47a6f7f6f066420e0db32fae0155d946ec369
Fernando Lopes <118869201+plasmus777@users.noreply.github.com>
2024-10-28 21:06:03 -0300

    Upload do Projeto Spring

---
+++ src/main/java/com/github/plasmus777/carrosAluguel/CarrosAluguelApplication.java
@@ -0,0 +1,13 @@
+package com.github.plasmus777.carrosAluguel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrosAluguelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrosAluguelApplication.class, args);
+	}
+
+}
